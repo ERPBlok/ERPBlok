ca11dc1366566f18a552af333878e75a871d0aed
jssuzanne <js.suzanne@gmail.com>
2022-03-02 17:46:41 +0100

    Added mixins

--- erpblok/erpblok/models/__init__.py
+++ erpblok/erpblok/models/__init__.py
@@ -1,4 +1,6 @@
 def import_declaration_module(reload=None):
     from . import core
+    from . import mixins
     if reload is not None:
         reload(core)
+        reload(mixins)
---
+++ erpblok/erpblok/models/mixins.py
@@ -0,0 +1,45 @@
+from anyblok import Declarations
+from anyblok.column import Integer, String, Country, Selection
+
+
+register = Declarations.register
+Mixin = Declarations.Mixin
+
+
+@register(Mixin)
+class ERPBlokAddress:
+
+    id = Integer(primary_key=True)
+    name = String()
+    street = String(nullable=False)
+    street_2 = String()
+    street_3 = String()
+    zip_code = String(nullable=False)
+    city = String(nullable=False)
+    country = Country(nullable=False)
+
+    @classmethod
+    def before_update_orm_event(cls, mapper, connection, target):
+        country_validator_method = f"country_validator_{target.country.alpha2}"
+        if hasattr(target, country_validator_method):
+            getattr(target, country_validator_method)()
+
+
+@register(Mixin)
+class ERPBlokContact:
+
+    id = Integer(primary_key=True)
+    type = Selection(selections="get_types", nullable=False)
+    value = String(nullable=False)
+    label = String(nullable=False)
+
+    @classmethod
+    def get_types(cls):
+        return {
+        }
+
+    @classmethod
+    def before_update_orm_event(cls, mapper, connection, target):
+        type_validator_method = f"type_validator_{target.type}"
+        if hasattr(target, type_validator_method):
+            getattr(target, type_validator_method)()
