66dbca7ab2def40857e9c838af0b846a2a465a1c
jssuzanne <js.suzanne@gmail.com>
2022-03-03 14:50:19 +0100

    Improve contact

--- erpblok/erpblok/models/mixins.py
+++ erpblok/erpblok/models/mixins.py
@@ -1,5 +1,7 @@
 from anyblok import Declarations
-from anyblok.column import Integer, String, Country, Selection
+from anyblok.field import Function
+from anyblok.column import (
+    Integer, String, Country, Selection, PhoneNumber, URL, Email)
 
 
 register = Declarations.register
@@ -31,13 +33,46 @@ class ERPBlokContact:
 
     id = Integer(primary_key=True)
     type = Selection(selections="get_types", nullable=False)
-    value = String(nullable=False)
+    value = Function(fget="get_value")
     label = String(nullable=False)
+    phone = PhoneNumber()
+    url = URL()
+    email = Email()
+
+    def get_value(self):
+        if self.type in ('phone', 'mobile', 'fax'):
+            return self.phone
+
+        if self.type in ('website',):
+            return self.url
+
+        if self.type in ('email',):
+            return self.email
 
     @classmethod
     def get_types(cls):
         return {
+            'phone': 'Phone',
+            'mobile': 'Mobile',
+            'fax': 'Fax',
+            'email': 'E-Mail',
+            'website': 'Website',
         }
+
+    def type_validator_phone(self):
+        if not self.phone:
+            raise Exception('the field phone is required')
+
+    type_validator_mobile = type_validator_phone
+    type_validator_fax = type_validator_phone
+
+    def type_validator_email(self):
+        if not self.email:
+            raise Exception('the field email is required')
+
+    def type_validator_website(self):
+        if not self.url:
+            raise Exception('the field url is required')
 
     @classmethod
     def before_update_orm_event(cls, mapper, connection, target):
